cc29421612533c4868450de507d4cdc2d09ae91a
FilipeVMartins <filipe_2021@hotmail.com>
2019-12-20 20:57:42 -0300

    AdicionarUI_DeletarMensagens-correcao

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -8,19 +8,22 @@
     {{Form::submit('Deletar Mensagens Selecionadas', ["class" => "btn btn-danger"])}}
         <br>
         <br>
+        <?php $i=0; ?>
         @foreach($messages as $message)
+
             <ul class="list-group">
                 <li class="list-group-item">Nome: {{$message->name}}</li>
                 <li class="list-group-item">Email: {{$message->email}}</li>
                 <li class="list-group-item">Mensagem: {{$message->message}}</li>
                 <li class="list-group-item">
                     <div class="custom-control custom-checkbox mb-3">
-                        <input type="checkbox" class="custom-control-input" id="customCheck" name="example1" value="{{$message->id}}">
-                        <label class="custom-control-label" for="customCheck">Marque a caixa para selecionar a mensagem acima para exclusão.</label>
+                        <input type="checkbox" class="custom-control-input" id="customCheck{{$i}}" name="{{$i}}" value="{{$message->id}}">
+                        <label class="custom-control-label" for="customCheck{{$i}}">Marque a caixa para selecionar a mensagem acima para exclusão.</label>
                     </div>
                 </li>
             </ul>
             <br>
+            <?php $i++ ?>
         @endforeach
     {{Form::submit('Deletar Mensagens Selecionadas', ["class" => "btn btn-danger"])}}
     {!! Form::close() !!}
