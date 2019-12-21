7aba8e0af5ca3c277d2e6f064474c844c23b8ae5
FilipeVMartins <filipe_2021@hotmail.com>
2019-12-20 22:44:29 -0300

    Add_metodo_deletMessages

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -5,21 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Message;
 
-class MessagesController extends Controller
-{
+class MessagesController extends Controller{
     public function submit (Request $request){
         $this->validate($request, [
             "name" => "required",
             "email"=> "required",
         ]);
         
-        // Create a New Message
+        // Criar nova mensagem
         $message = new Message;
         $message->name = $request->input("name");
         $message->email = $request->input("email");
         $message->message = $request->input("message");
         
-        // Save Message
+        // Salvar mensagem criada
         $message -> save();
         
         // Redirect
@@ -36,5 +35,20 @@ class MessagesController extends Controller
     public function getMessages(){
         $messages = Message::all();
         return view("messages")->with("messages", $messages);
+    }
+    
+    public function deletMessages(Request $request){
+        //obter os ID's para deletar
+        $mensagensids = $request->all();
+        //verificar se tem algo para deletear, usei >1 para começar depois do token.
+        if (count($mensagensids)>1){
+            //percorrer a array dos ids e deletar.
+            foreach ($mensagensids as $messageid){
+                Message::destroy($messageid);
+            }
+        } else{
+            return redirect("/messages") -> with("success", "Nenhuma mensagem selecionada para exclusão");
+        }
+        return redirect("/messages") -> with("success", "As mensagens foram excluídas com sucesso.");
     }
 }
